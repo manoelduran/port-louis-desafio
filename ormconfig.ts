bcbaa489a572aeba3846179b2c9e834d22ed7757
Manoel Duran <manoel.duran@hotmail.com>
2023-04-02 19:28:53 -0300

    fixed ormconfig

---
+++ ormconfig.ts
@@ -0,0 +1,32 @@
+'reflect-metadata';
+import { DataSource, DataSourceOptions } from "typeorm";
+import { SeederOptions } from 'typeorm-extension';
+import MainSeeder from './src/shared/infra/typeorm/seeds/MainSeeder';
+
+const options = {
+   "type": "postgres",
+   "host": "database",
+   "port": 5432,
+   "username": "portlouistest",
+   "password": "portlouistest123",
+   "database": "portlouisdb",
+   "synchronize": true,
+   "migrationsRun": false,
+   "logging": true,
+   "migrations": ["./src/shared/infra/typeorm/migrations/*.ts"],
+   "seeds": [MainSeeder],
+   "entities": ["./src/modules/**/infra/persistence/entity/*.ts"],
+   "cli": {
+      "migrationsDir": "./src/shared/infra/typeorm/migrations"
+   }
+} as DataSourceOptions & SeederOptions
+
+export const PostgresDataSource: DataSource = new DataSource(options);
+
+module.exports = {
+   PostgresDataSource
+}
+
+
+
+
